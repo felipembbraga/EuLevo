6c42bc9fc804bbed3dd520f848d52d5726337f79
Felipe Mendes Braga <felipe.mendes.braga@gmail.com>
2016-12-29 15:55:44 -0300

    teste

--- EuLevo/settings.py
+++ EuLevo/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
+# Test post-receive
+
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
