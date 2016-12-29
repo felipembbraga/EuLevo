1e63494e79e63ec119ee22cfe39932680aea96f8
Felipe Mendes Braga <felipe.mendes.braga@gmail.com>
2016-12-29 16:27:41 -0300

    gitignore atualizado

--- EuLevo/settings.py
+++ EuLevo/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
-# alteração para tag 0.0.6
+# alteração para tag v0.0.6
 
 
 import os
