f1c13b56d67d974a71f62aa4e3407d7387f7a8bb
Felipe Mendes Braga <felipe.mendes.braga@gmail.com>
2017-01-20 14:29:25 -0300

    busca de viagem concertada

--- eulevo/views/travel.py
+++ eulevo/views/travel.py
@@ -50,7 +50,7 @@ class TravelViewSet(EuLevoModelViewSet):
                 lookups = {
                     'destiny__distance_lte': (package.destiny, 10000),
                     'weight_range__gte': package.weight_range,
-                    'dt_travel__lte': datetime.date.today()
+                    'dt_travel__gte': datetime.date.today()
 
                 }
                 if hasattr(request.user, 'userpoint'):
